314793e6b7290e46ad3a8834b106ce77bca721d9
Codster dev <deepaksingh.desire@gmail.com>
2024-01-25 16:37:51 +0530

    add

--- Ashok Sir - Backend Development/Core java/Notes/Packages ExceptionHandlingNotes.java
+++ Ashok Sir - Backend Development/Core java/Notes/Packages ExceptionHandlingNotes.java
@@ -44,3 +44,81 @@ public class car {
 }
 
 ------------------------------------------------------------------------------------------------------------
+
+
+    
+    .                                 -->    day 24  (24 - 1 - 2024)
+
+
+
+.                                                 C O R E   J A V A
+
+
+
+lec -51
+
+
+
+        exception handling 
+        //try
+        //catch
+        //finally
+        //throw
+        //throws
+
+
+programm
+==========
+
+
+throws
+========
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class test2 {
+    public static void main(String[] args) throws FileNotFoundException , ClassNotFoundException { // insted of this we these two we can give --> Exception only all checked Exception will get ignored
+        FileReader fr = new FileReader("abc.txt");
+          Class.forName("");      
+    }
+}
+----------------------------------------------------------------
+
+public class test {
+    public static void main(String[] args) {
+        try{
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            System.out.println("this line will not be executed");
+        }catch (ArithmeticException e){
+            System.out.println("Catch block : divided by zero");
+
+        }finally {
+            System.out.println("I am from  finnally no one can stop me to to execute");
+        }
+        System.out.println("After execution is handled");
+      
+    }
+}
+
+
+-----------------------------------------------------------------------------------------------     
+
+
+    
+    .                                 -->    day 25  (25 - 1 - 2024)
+
+
+
+.                                                 C O R E   J A V A
+
+
+
+lec -52
+
+                Exception handling completed
+                
+
+
+---------------------------------------------------------------------------------------------
