c871085a5b649d2f7e68d9ce635bd112b2a551b9
Codster dev <deepaksingh.desire@gmail.com>
2024-02-03 01:56:20 +0530

    add

--- Ashok Sir - Backend Development/Core java/Notes/Multithreading.java
+++ Ashok Sir - Backend Development/Core java/Notes/Multithreading.java
@@ -40,6 +40,98 @@ lec -60
         Muti threading
 
 
+        program  -- > using thread class extends
+        ========
+
+-----------------------------------------------------------------
+
+        package date31jan_2024;
+
+public class pro1 extends Thread {
+
+    public void run(){
+        System.out.println("run() - executed");
+        try{
+            Thread.sleep(3000);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("execution ended");
+    }
+
+    public static void main(String[] args) {
+        pro1 p = new pro1();
+        Thread t = new Thread(p);
+            t.start();
+    }
+}
+--------------------------------------------------------------------------------
+
+using interface  -- > runable
+
+
+
+
+package date31jan_2024;
+
+public class pro1 implements Runnable {
+
+    public void run(){
+        System.out.println("run() - executed");
+        try{
+            Thread.sleep(3000);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("execution ended");
+    }
+
+    public static void main(String[] args) {
+        pro1 p = new pro1();
+        Thread t = new Thread(p);
+            t.start();
+    }
+}
+--------------------------------------------------------------
+
+    MOST at reccommaned way to is to create thread is -->  using callable interface 
+
+
+
+    package date31jan_2024;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class pro1 implements Callable {
+
+    public Object call() {
+        System.out.println("run() - executed");
+
+
+        return "hi";
+    }
+
+    public static void main(String[] args) {
+        pro1 p = new pro1();
+        ExecutorService es = Executors.newFixedThreadPool(5);
+
+
+        for (int i = 0; i <=30 ; i++) {
+            es.submit(p);
+
+        }
+        es.shutdown();
+
+    }
+}
+
+
+        
+
 
 
 --------------------------------------------------------------------------------------------------------
